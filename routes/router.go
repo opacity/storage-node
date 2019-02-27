bbbae712f8ee1107664c5d2b2e05838dc56ec29a
Rebel Fornea <rebelfornea@gmail.com>
2019-02-27 16:24:33 -0500

    updates

--- routes/router.go
+++ routes/router.go
@@ -71,7 +71,7 @@ func setupV1Paths(v1Router *gin.RouterGroup) {
 	v1Router.GET(AccountsPath+"/:accountID", CheckAccountPaymentStatusHandler())
 
 	v1Router.POST(MetadataPath, UpdateMetadataHandler())
-	v1Router.GET(MetadataPath+":/metadataKey", GetMetadataHandler())
+	v1Router.GET(MetadataPath+"/:metadataKey", GetMetadataHandler())
 
 	v1Router.POST("/trial-upload", func(c *gin.Context) {
 		c.JSON(http.StatusOK, "stub for doing a trial upload")
