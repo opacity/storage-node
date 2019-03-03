bb4fcbbefbcd9551e80b3bd61357c07bd81b5202
Peisen Zhao <philipzhao@Peisens-MBP.fios-router.home>
2019-03-03 16:30:20 -0500

    update

--- models/s3_life_cycle.go
+++ models/s3_life_cycle.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/jinzhu/gorm"
+	"github.com/opacity/storage-node/utils"
 )
 
 /*The life cycle of a particular object inside the bucket. */
@@ -11,4 +12,13 @@ type S3ObjectLifeCycle struct {
 	BucketName  string `gorm:"primary_key"`
 	ObjectName  string `gorm:"primary_key"`
 	ExpiredTime time.Time
+}
+
+func ExpireObject(objectName string) error{
+	s := S3ObjectLifeCycle{
+		BucketName : utils.Env.BucketName,
+		ObjectName: objectName,
+		ExpiredTime: time.Now().Add(time.Hour * 1)
+	}
+	return DB.Create(&s).Error
 }
