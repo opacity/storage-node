8e7b37c83ee96f6662b37e16a45c8c1b4debf849
Philip Zhao <pzhao5@users.noreply.github.com>
2019-03-03 18:04:14 -0500

    Setup free_upload endpoint (#53)
    * update
    
    * update

---
+++ routes/free_upload_file.go
@@ -0,0 +1,41 @@
+package routes
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/opacity/storage-node/utils"
+)
+
+type freeUploadFileReq struct {
+	UploadID string `json:"uploadID" binding:"required"`
+	FileData string `json:"fileData" binding:"required"`
+}
+
+type freeUploadFileRes struct {
+	Status string `json:"status"`
+}
+
+func FreeUploadFileHandler() gin.HandlerFunc {
+	return gin.HandlerFunc(freeUploadFile)
+}
+
+func freeUploadFile(c *gin.Context) {
+	request := freeUploadFileReq{}
+	if err := utils.ParseRequestBody(c.Request, &request); err != nil {
+		err = fmt.Errorf("bad request, unable to parse request body:  %v", err)
+		BadRequestResponse(c, err)
+		return
+	}
+
+	objectKey := fmt.Sprintf("%s%s", "free_upload/", request.UploadID)
+
+	if err := utils.SetDefaultBucketObject(objectKey, request.FileData); err != nil {
+		InternalErrorResponse(c, err)
+		return
+	}
+
+	OkResponse(c, uploadFileRes{
+		Status: "File is uploaded",
+	})
+}
