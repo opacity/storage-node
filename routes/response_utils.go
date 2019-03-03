c0f7d779e152e44e40c22572585bcf5424ccf0d4
Philip Zhao <pzhao5@users.noreply.github.com>
2019-03-03 16:48:35 -0500

    Check Object Existed and account exist before returning the caller (#52)
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- routes/response_utils.go
+++ routes/response_utils.go
@@ -8,30 +8,34 @@ import (
 	"github.com/opacity/storage-node/utils"
 )
 
-func InternalError(c *gin.Context, err error) {
+func InternalErrorResponse(c *gin.Context, err error) {
 	c.AbortWithStatusJSON(http.StatusInternalServerError, err.Error())
 }
 
-func BadRequest(c *gin.Context, err error) {
+func BadRequestResponse(c *gin.Context, err error) {
 	c.AbortWithStatusJSON(http.StatusBadRequest, err.Error())
 }
 
-func ServiceUnavailable(c *gin.Context, err error) {
+func ServiceUnavailableResponse(c *gin.Context, err error) {
 	c.AbortWithStatusJSON(http.StatusServiceUnavailable, err.Error())
+}
+
+func AccountNotFoundResponse(c *gin.Context, id string) {
+	c.AbortWithStatusJSON(http.StatusNotFound, fmt.Sprintf("no account with that id: %s", id))
 }
 
 func Forbidden(c *gin.Context, err error) {
 	c.AbortWithStatusJSON(http.StatusForbidden, err.Error())
 }
 
-func NotFound(c *gin.Context, err error) {
+func NotFoundResponse(c *gin.Context, err error) {
 	c.AbortWithStatusJSON(http.StatusNotFound, err.Error())
 }
 
 func OkResponse(c *gin.Context, response interface{}) {
 	if err := utils.Validator.Struct(response); err != nil {
 		err = fmt.Errorf("could not create a valid response:  %v", err)
-		BadRequest(c, err)
+		BadRequestResponse(c, err)
 		return
 	}
 	c.JSON(http.StatusOK, response)
