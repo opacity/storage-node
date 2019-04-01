095c1186ed3827757f6c710943931dfb7a21d772
R. L. Fornea <rebelfornea@gmail.com>
2019-04-01 17:05:33 -0400

    Update metadata.go (#83)
    * Update metadata.go
    
    * fix logic

--- routes/metadata.go
+++ routes/metadata.go
@@ -15,7 +15,7 @@ type updateMetadataReq struct {
 }
 
 type getMetadataRes struct {
-	Metadata string `json:"Metadata" binding:"required"`
+	Metadata string `json:"Metadata"`
 }
 
 /*GetMetadataHandler is a handler for getting the file metadata*/
