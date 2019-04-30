f769daab14276cfc0152fb917632be01a32deb8e
Philip Zhao <pzhao5@users.noreply.github.com>
2019-04-29 22:04:42 -0400

    Add test for s3 file deleter (#116)
    * update
    
    * update
    
    * update
    
    * update
    
    * Convert File to completedFile (#115)
    
    * update
    
    * update
    
    * update
    
    * missing initialization (#117)
    
    * Feature/cryptographic verification (#118)
    
    * initial commit
    
    * update create account endpoint, abstract some verification logic away.
    
    * add verification to delete_file endpoint, small fix to accounts endpoint.
    
    * apply cryptographic verification on upload endpoint, other fixes.
    
    * update
    
    * update
    
    * update
    
    * update

---
+++ jobs/s3_deleter_test.go
@@ -0,0 +1,42 @@
+package jobs
+
+import (
+	"testing"
+	"time"
+
+	"github.com/opacity/storage-node/models"
+	"github.com/opacity/storage-node/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Init_S3_Deleter(t *testing.T) {
+	utils.SetTesting("../.env")
+	models.Connect(utils.Env.DatabaseURL)
+}
+
+func Test_DeleteAllExpiredCompletedFiles(t *testing.T) {
+	s := models.CompletedFile{
+		FileID:    "foo1",
+		ExpiredAt: time.Date(2009, 1, 1, 12, 0, 0, 0, time.UTC), // past
+	}
+	assert.Nil(t, models.DB.Create(&s).Error)
+	s = models.CompletedFile{
+		FileID:    "foo2",
+		ExpiredAt: time.Date(2010, 1, 1, 12, 0, 0, 0, time.UTC), // past
+	}
+	assert.Nil(t, models.DB.Create(&s).Error)
+	s = models.CompletedFile{
+		FileID:    "foo3",
+		ExpiredAt: time.Now().AddDate(0, 1, 0), // future
+	}
+	assert.Nil(t, models.DB.Create(&s).Error)
+
+	testSubject := s3Deleter{}
+	testSubject.Run()
+
+	result := []models.CompletedFile{}
+	assert.Nil(t, models.DB.Find(&result).Error)
+
+	assert.Equal(t, 1, len(result))
+	assert.Equal(t, "foo3", result[0].FileID)
+}
