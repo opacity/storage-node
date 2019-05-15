cb79bf95815a17bb6e7afc74338defbc2f058c15
Peisen Zhao <philipzhao@Peisens-MBP.fios-router.home>
2019-05-14 23:09:40 -0400

    update

--- routes/verification_utils_test.go
+++ routes/verification_utils_test.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"bytes"
 	"mime/multipart"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
@@ -17,59 +18,59 @@ type testRequestObject struct {
 type testVerifiedRequest struct {
 	verification
 	requestBody
-	fooValue      string `form:"foo"`
+	strValue      string `form:"str"`
 	fileObject    string `formFile:"file"`
 	requestObject testRequestObject
 }
 
 type testSetRequest struct {
-	fooValue    string `form:"foo"`
-	fileObject  string `formFile:"file"`
-	randomValue string
+	strValue   string `form:"str"`
+	fileObject string `formFile:"file"`
+	emptyValue string
 }
 
-func (v *testRequest) getObjectRef() interface{} {
+func (v *testVerifiedRequest) getObjectRef() interface{} {
 	return &v.requestObject
 }
 
 func Test_verifyAndParseFormRequestWithVerifyRequest(t *testing.T) {
-	c, _ := gin.CreateTestContext(httptest.NewRecorder())
-
 	body := new(bytes.Buffer)
 	mw := multipart.NewWriter(body)
 	mw.WriteField("signature", "signature")
 	mw.WriteField("publicKey", "public")
 	mw.WriteField("requestBody", "body")
-	mw.WriteField("foo", "foo")
-	w, err := mw.CreateFormFile("file", "test")
+	mw.WriteField("str", "strV")
+	w, _ := mw.CreateFormFile("file", "test")
 	w.Write([]byte("test"))
+	mw.Close()
 
+	c, _ := gin.CreateTestContext(httptest.NewRecorder())
 	c.Request, _ = http.NewRequest("POST", "/", body)
 
 	request := testVerifiedRequest{}
 	err := verifyAndParseFormRequest(&request, c)
 
 	assert.Nil(t, err)
-	assert.Equal(t, request.fooValue, "foo")
+	assert.Equal(t, request.strValue, "strV")
 	assert.Equal(t, request.requestObject.data, "body")
 }
 
 func Test_verifyAndParseFormRequestWithNormalRequest(t *testing.T) {
-	c, _ := gin.CreateTestContext(httptest.NewRecorder())
-
 	body := new(bytes.Buffer)
 	mw := multipart.NewWriter(body)
-	mw.WriteField("foo", "foo")
-	w, err := mw.CreateFormFile("file", "test")
+	mw.WriteField("str", "strV")
+	w, _ := mw.CreateFormFile("file", "test")
 	w.Write([]byte("test"))
+	mw.Close()
 
+	c, _ := gin.CreateTestContext(httptest.NewRecorder())
 	c.Request, _ = http.NewRequest("POST", "/", body)
 
 	request := testSetRequest{}
 	err := verifyAndParseFormRequest(&request, c)
 
 	assert.Nil(t, err)
-	assert.Equal(t, request.fooValue, "foo")
+	assert.Equal(t, request.strValue, "strV")
 	assert.Equal(t, request.fileObject, "test")
-	assert.Equal(t, request.randomValue, "")
+	assert.Equal(t, request.emptyValue, "")
 }
