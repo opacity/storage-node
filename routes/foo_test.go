5145a463065fd73ba0c61c46c2dcec20459612a2
Peisen Zhao <philipzhao@Peisens-MBP.fios-router.home>
2019-05-30 21:13:36 -0400

    update

---
+++ routes/foo_test.go
@@ -0,0 +1,22 @@
+package routes
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_C(t *testing.T) {
+	assert.Nil(t, "C")
+}
+
+func Test_B(t *testing.T) {
+	assert.Nil(t, "B")
+}
+
+func Test_Init_Foo(t *testing.T) {
+	assert.Nil(t, "Init Foo")
+}
+
+func Test_A(t *testing.T) {
+	assert.Nil(t, "A")
+}
