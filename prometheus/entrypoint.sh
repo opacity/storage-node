44e444484ba0c3907b00a5de95923fa02c788194
R. L. Fornea <rebelfornea@gmail.com>
2019-06-17 17:44:43 -0400

    prometheus update (#223)

---
+++ prometheus/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sed -i -r "s/@@ADMIN_USER@@/$1/" prometheus/prometheus.yml
+sed -i -r "s/@@ADMIN_PASSWORD@@/$2/" prometheus/prometheus.yml
+
+/bin/prometheus --config.file=prometheus/prometheus.yml --web.external-url=http://localhost:12321/prometheus
