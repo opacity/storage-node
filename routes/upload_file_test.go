dc0da2cc647533ae5145a8d3d9ce919b41fe8387
Peisen Zhao <philipzhao@Peisens-MBP.fios-router.home>
2019-06-23 17:07:16 -0400

    update

--- routes/upload_file_test.go
+++ routes/upload_file_test.go
@@ -150,60 +150,6 @@ func Test_Upload_Completed_No_In_Order(t *testing.T) {
 	utils.DeleteDefaultBucketObject(models.GetFileDataKey(fileId))
 }
 
-// func Test_Upload_File_Completed_File_Is_Deleted(t *testing.T) {
-// 	// TODO: Update tests to work with multipart-form requests
-// 	t.Skip()
-// 	models.DeleteAccountsForTest(t)
-// 	models.DeleteCompletedFilesForTest(t)
-// 	models.DeleteFilesForTest(t)
-
-// 	uploadBody := ReturnValidUploadFileBodyForTest(t)
-// 	uploadBody.PartIndex = models.FirstChunkIndex
-
-// 	chunkData := ReturnChunkDataForTest(t)
-// 	chunkDataPart1 := chunkData[0:utils.MaxMultiPartSizeForTest]
-// 	chunkDataPart2 := chunkData[utils.MaxMultiPartSizeForTest:]
-
-// 	privateKey, err := utils.GenerateKey()
-// 	assert.Nil(t, err)
-// 	request := ReturnValidUploadFileReqForTest(t, uploadBody, privateKey)
-// 	request.ChunkData = string(chunkDataPart1)
-// 	accountID, err := utils.HashString(request.PublicKey)
-// 	assert.Nil(t, err)
-// 	account := CreatePaidAccountForTest(t, accountID)
-
-// 	objectKey := InitUploadFileForTest(t, request.PublicKey, uploadBody.FileHandle, models.FirstChunkIndex + 1)
-// 	w := UploadFileHelperForTest(t, request)
-
-// 	if w.Code != http.StatusOK {
-// 		t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
-// 	}
-// 	uploadBody.PartIndex++
-
-// 	request = ReturnValidUploadFileReqForTest(t, uploadBody, privateKey)
-// 	request.ChunkData = string(chunkDataPart2)
-
-// 	w = UploadFileHelperForTest(t, request)
-
-// 	if w.Code != http.StatusOK {
-// 		t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
-// 	}
-
-// 	file, _ := models.GetFileById(uploadBody.FileHandle)
-// 	assert.True(t, len(file.FileID) == 0)
-
-// 	updatedAccount, err := models.GetAccountById(account.AccountID)
-// 	assert.Nil(t, err)
-
-// 	assert.True(t, updatedAccount.StorageUsedInByte > account.StorageUsedInByte)
-
-// 	completedFile, _ := models.GetCompletedFileByFileID(uploadBody.FileHandle)
-// 	assert.Equal(t, completedFile.FileID, uploadBody.FileHandle)
-
-// 	err = utils.DeleteDefaultBucketObject(objectKey)
-// 	assert.Nil(t, err)
-// }
-
 func initFileUpload(t *testing.T, endIndex int, privateKey *ecdsa.PrivateKey) string {
 	req, uploadObj := createValidInitFileUploadRequest(t, 123, endIndex, privateKey)
 	form := map[string]string{
