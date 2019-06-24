478b243126b5d5ac8a66718db5564a358e771e38
Rebel Fornea <rebelfornea@gmail.com>
2019-06-24 14:59:47 -0400

    remove check, fix this problem manually instead.

--- jobs/token_collector.go
+++ jobs/token_collector.go
@@ -3,7 +3,6 @@ package jobs
 import (
 	"github.com/opacity/storage-node/models"
 	"github.com/opacity/storage-node/services"
-	"github.com/opacity/storage-node/utils"
 )
 
 type tokenCollector struct{}
@@ -26,11 +25,6 @@ func (t tokenCollector) Runnable() bool {
 
 func runCollectionSequence(accounts []models.Account) {
 	for _, account := range accounts {
-		if utils.FreeModeEnabled() {
-			err := models.DB.Model(&account).Update("payment_status", models.PaymentRetrievalComplete).Error
-			utils.LogIfError(err, nil)
-			continue
-		}
 		models.PaymentCollectionFunctions[account.PaymentStatus](account)
 	}
 }
