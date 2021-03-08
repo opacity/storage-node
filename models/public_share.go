5e09fce10754d8887798a20eeac4037ccecc2c8f
Mihai Iorga <mihai7221@gmail.com>
2021-03-08 13:11:17 +0200

    add FileID as unique index for PublicShare

--- models/public_share.go
+++ models/public_share.go
@@ -4,10 +4,10 @@ import "time"
 
 // PublicShare ...
 type PublicShare struct {
-	PublicID   string    `gorm:"primary_key" json:"public_id" binding:"required"`
+	PublicID   string    `gorm:"primary_key;autoIncrement:false" json:"public_id" binding:"required"`
 	CreatedAt  time.Time `json:"createdAt"`
 	ViewsCount int       `json:"views_count"`
-	FileID     string    `gorm:"foreignKey:FileID"`
+	FileID     string    `gorm:"UNIQUE_INDEX:idx_publicshare;" json:"file_id"`
 }
 
 // GetPublicShareByID returns the public share. If not found, return nil without error.
@@ -15,4 +15,9 @@ func GetPublicShareByID(publicID string) (PublicShare, error) {
 	publicShare := PublicShare{}
 	err := DB.Where("public_id = ?", publicID).First(&publicShare).Error
 	return publicShare, err
+}
+
+// UpdateViewsCount increments the views count of a PublicShare by 1
+func (publicShare *PublicShare) UpdateViewsCount() error {
+	return DB.Model(&publicShare).Update("views_count", publicShare.ViewsCount+1).Error
 }
