ac8648149324ae9519ff91d3147206dee08d51e6
Mihai Iorga <mihai7221@gmail.com>
2021-03-09 16:45:48 +0200

    public share add validation

--- models/public_share.go
+++ models/public_share.go
@@ -1,13 +1,28 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+	"github.com/opacity/storage-node/utils"
+)
 
 // PublicShare ...
 type PublicShare struct {
 	PublicID   string    `gorm:"primary_key;autoIncrement:false" json:"public_id" binding:"required"`
 	CreatedAt  time.Time `json:"createdAt"`
 	ViewsCount int       `gorm:"not null" json:"views_count"`
-	FileID     string    `gorm:"UNIQUE_INDEX:idx_publicshare;not null" json:"file_id"`
+	FileID     string    `gorm:"UNIQUE_INDEX:idx_publicshare;not null" json:"file_id" binding:"required,len=64" minLength:"64" maxLength:"64"`
+}
+
+/*BeforeCreate - callback called before the row is created*/
+func (publicShare *PublicShare) BeforeCreate(scope *gorm.Scope) error {
+	return utils.Validator.Struct(publicShare)
+}
+
+/*BeforeUpdate - callback called before the row is updated*/
+func (publicShare *PublicShare) BeforeUpdate(scope *gorm.Scope) error {
+	return utils.Validator.Struct(publicShare)
 }
 
 // GetPublicShareByID returns the public share. If not found, return nil without error.
