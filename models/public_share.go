6ac3e420e28c515066fb25057ed37fae303aa2e7
Mihai Iorga <mihai7221@gmail.com>
2021-06-14 19:34:35 +0300

    DEV-48: fix Test_Public_Share_Empty_FileID_Fails

--- models/public_share.go
+++ models/public_share.go
@@ -12,14 +12,14 @@ import (
 
 // PublicShare ...
 type PublicShare struct {
-	PublicID      string    `gorm:"UNIQUE_INDEX:idx_publicshare;primary_key;autoIncrement:false" json:"public_id" binding:"required"`
+	PublicID      string    `gorm:"UNIQUE_INDEX:idx_publicshare;primary_key;autoIncrement:false" json:"public_id" validate:"required"`
 	CreatedAt     time.Time `json:"createdAt"`
 	ViewsCount    int       `gorm:"not null" json:"views_count"`
 	Title         string    `gorm:"not null;size:65535" json:"title"`
 	Description   string    `gorm:"not null;size:65535" json:"description"`
 	MimeType      string    `gorm:"not null;size:255" json:"mimeType"`
 	FileExtension string    `gorm:"not null;size:255" json:"fileExtension"`
-	FileID        string    `gorm:"not null" json:"file_id" binding:"required,len=64" minLength:"64" maxLength:"64"`
+	FileID        string    `gorm:"not null" json:"file_id" validate:"required,len=64" minLength:"64" maxLength:"64"`
 }
 
 // CreateShortlinkObj...
