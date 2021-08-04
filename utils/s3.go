1f0a108b660f53f957cc1111424ad313a5388a1f
Mihai Iorga <mihai7221@gmail.com>
2021-08-04 11:49:44 +0300

    Remove API Key and Secret dependency from S3 Session

--- utils/s3.go
+++ utils/s3.go
@@ -41,15 +41,10 @@ func init() {
 }
 
 func newS3Session() {
-	hasAwsCredentials := len(Env.AwsAccessKeyID) > 0 && len(Env.AwsSecretAccessKey) > 0 &&
-		len(Env.BucketName) > 0 && len(Env.AwsRegion) > 0
-	// Stub out the S3 if we don't have S3 access right.
-	if hasAwsCredentials {
-		svc = &s3Wrapper{
-			s3: s3.New(session.Must(session.NewSession())),
-		}
-	} else {
-		svc = &s3Wrapper{}
+	svc = &s3Wrapper{
+		s3: s3.New(session.Must(session.NewSessionWithOptions(session.Options{
+			SharedConfigState: session.SharedConfigEnable,
+		}))),
 	}
 }
 
