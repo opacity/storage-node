2aa11f145a7b571cba1b5c042601cd5141a9959a
Mihai Iorga <mihai7221@gmail.com>
2021-08-10 20:33:38 +0300

    output as string in app_start

--- deploy_scripts/app_start.sh
+++ deploy_scripts/app_start.sh
@@ -3,8 +3,8 @@
 cd /home/ubuntu/storage-node/
 
 GO_ENV=$(grep GO_ENV .env | cut -d '=' -f2)
-echo "SIA_API_PASSWORD=$(aws ssm get-parameter --name /storage-node/$GO_ENV/SIA_API_PASSWORD --with-decryption --output text --query Parameter.Value)" >> .env
-echo "SIA_WALLET_PASSWORD=$(aws ssm get-parameter --name /storage-node/$GO_ENV/SIA_WALLET_PASSWORD --with-decryption --output text --query Parameter.Value)" >> .env
+echo "SIA_API_PASSWORD=$(aws ssm get-parameter --name /storage-node/$GO_ENV/SIA_API_PASSWORD --with-decryption --query Parameter.Value)" >> .env
+echo "SIA_WALLET_PASSWORD=$(aws ssm get-parameter --name /storage-node/$GO_ENV/SIA_WALLET_PASSWORD --with-decryption --query Parameter.Value)" >> .env
 
 docker-compose up -d
 
