9014a66d9519358bbbb5e124f862b132a56da084
Mihai Iorga <mihai7221@gmail.com>
2021-08-12 08:31:51 +0300

    add sia_client and upload file func

---
+++ utils/sia_client.go
@@ -0,0 +1,41 @@
+package utils
+
+import (
+	"strings"
+
+	sia_modules "gitlab.com/NebulousLabs/Sia/modules"
+	sia_client "gitlab.com/NebulousLabs/Sia/node/api/client"
+)
+
+const (
+	dataPieces   = 0
+	parityPieces = 0
+)
+
+var siaClient *sia_client.Client
+
+func InitSiaClient() {
+	opts, err := sia_client.DefaultOptions()
+	if err != nil {
+		LogIfError(err, nil)
+	}
+
+	opts.Password = Env.SiaApiPassword
+	siaClient = sia_client.New(opts)
+}
+
+func UploadSiaFile(fileData, fileSiaPath string, deleteIfExisting bool) error {
+	siaPath, err := sia_modules.NewSiaPath(fileSiaPath)
+	if err != nil {
+		return err
+	}
+
+	fileDataReader := strings.NewReader(fileData)
+
+	err = siaClient.RenterUploadStreamPost(fileDataReader, siaPath, dataPieces, parityPieces, deleteIfExisting)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
