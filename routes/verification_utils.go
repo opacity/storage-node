9aa8f7bd039d0ea9be528332f0ebd910120848a8
Mihai Iorga <mihai7221@gmail.com>
2021-08-12 08:08:02 +0300

    export SignatureDidNotMatchResponse

--- routes/verification_utils.go
+++ routes/verification_utils.go
@@ -18,7 +18,7 @@ import (
 )
 
 const (
-	signatureDidNotMatchResponse = "signature did not match"
+	SignatureDidNotMatchResponse = "signature did not match"
 	errVerifying                 = "error verifying signature"
 	marshalError                 = "bad request, unable to marshal request body: "
 	notAuthorizedResponse        = "you are not authorized to access or modify this resource"
@@ -233,7 +233,7 @@ func verifyRequest(hash []byte, verificationData verification, c *gin.Context) e
 	}
 
 	if verified != true {
-		return ForbiddenResponse(c, errors.New(signatureDidNotMatchResponse))
+		return ForbiddenResponse(c, errors.New(SignatureDidNotMatchResponse))
 	}
 	return nil
 }
