66c0f10bd5652bac775b270fff4cffc83c164e98
Mihai Iorga <mihai7221@gmail.com>
2021-10-12 09:27:44 +0300

    add admin sia get

--- routes/admin-sia.go
+++ routes/admin-sia.go
@@ -1,6 +1,23 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/opacity/storage-node/utils"
+	sia_types "gitlab.com/NebulousLabs/Sia/types"
+)
+
+type AdminSiaAllowance struct {
+	AllowanceFunds   string
+	ExpectedStorage  string
+	Period           string
+	Hosts            uint64
+	RenewWindow      string
+	ExpectedDownload string
+	ExpectedUpload   string
+}
 
 func AdminSiaAllowanceGetHandler() gin.HandlerFunc {
 	return ginHandlerFunc(adminSiaAllowanceGet)
@@ -11,11 +28,42 @@ func AdminSiaAllowanceChangeHandler() gin.HandlerFunc {
 }
 
 func adminSiaAllowanceGet(c *gin.Context) error {
+	return siaAllowanceGet(c, "")
+}
+
+func adminSiaAllowanceChange(c *gin.Context) error {
 
 	return nil
 }
 
-func adminSiaAllowanceChange(c *gin.Context) error {
+func siaAllowanceGet(c *gin.Context, notificationMessage string) error {
+	renter := utils.GetSiaRenter()
+
+	fundsH := renter.Settings.Allowance.Funds.Div64(1e18)
+	funds := fundsH.Div64(1e6).String()
+
+	adminSiaAllowance := AdminSiaAllowance{
+		AllowanceFunds:   funds,
+		ExpectedStorage:  fmt.Sprintf("%.2f", float64(renter.Settings.Allowance.ExpectedStorage)/1e12),
+		Period:           fmt.Sprintf("%.1f", float64(renter.Settings.Allowance.Period)/float64(sia_types.BlocksPerWeek)),
+		Hosts:            renter.Settings.Allowance.Hosts,
+		RenewWindow:      fmt.Sprintf("%.1f", float64(renter.Settings.Allowance.RenewWindow)/float64(sia_types.BlocksPerWeek)),
+		ExpectedDownload: fmt.Sprintf("%.2f", float64(renter.Settings.Allowance.ExpectedDownload)*float64(sia_types.BlocksPerMonth)/1e12),
+		ExpectedUpload:   fmt.Sprintf("%.2f", float64(renter.Settings.Allowance.ExpectedUpload)*float64(sia_types.BlocksPerMonth)/1e12),
+	}
+
+	totalSpent, unspentAllocated, unspentUnallocated := renter.FinancialMetrics.SpendingBreakdown()
+
+	c.HTML(http.StatusOK, "sia-allowance.tmpl", gin.H{
+		"title":               "Sia allowance",
+		"allowance":           adminSiaAllowance,
+		"totalSpent":          totalSpent.HumanString(),
+		"contractFees":        renter.FinancialMetrics.ContractFees.HumanString(),
+		"storageSpending":     renter.FinancialMetrics.StorageSpending.HumanString(),
+		"unspentAllocated":    unspentAllocated.HumanString(),
+		"unspentUnallocated":  unspentUnallocated.HumanString(),
+		"notificationMessage": "",
+	})
 
 	return nil
 }
