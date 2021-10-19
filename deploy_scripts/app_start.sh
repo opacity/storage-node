d200ad068962c321c5e85a0f4ffe3add3a18d526
Mihai Iorga <mihai7221@gmail.com>
2021-10-19 18:21:43 +0300

    update deploy script

--- deploy_scripts/app_start.sh
+++ deploy_scripts/app_start.sh
@@ -2,8 +2,4 @@
 
 cd /home/ubuntu/storage-node/
 
-GO_ENV=$(grep GO_ENV .env | cut -d '=' -f2)
-echo "ADMIN_USER=$(aws ssm get-parameter --name /storage-node/$GO_ENV/ADMIN_USER --with-decryption --output text --query Parameter.Value)" >> .env
-echo "ADMIN_PASSWORD=$(aws ssm get-parameter --name /storage-node/$GO_ENV/ADMIN_PASSWORD --with-decryption --output text --query Parameter.Value)" >> .env
-
-docker-compose up --build -d
+docker-compose up -d
