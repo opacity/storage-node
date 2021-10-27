12931395d9cd05d170fe07cf15d5924b351cdf95
Mihai Iorga <mihai7221@gmail.com>
2021-10-27 09:18:46 +0300

    docker login and docker pull in app_start

--- deploy_scripts/app_start.sh
+++ deploy_scripts/app_start.sh
@@ -14,8 +14,9 @@ VERSION=$(grep VERSION .env | cut -d '=' -f2)
 echo "ADMIN_USER=$(aws ssm get-parameter --name /storage-node/$GO_ENV/ADMIN_USER --with-decryption --output text --query Parameter.Value)" >> .env
 echo "ADMIN_PASSWORD=$(aws ssm get-parameter --name /storage-node/$GO_ENV/ADMIN_PASSWORD --with-decryption --output text --query Parameter.Value)" >> .env
 
-$(aws ecr get-login --region $AWS_REGION --no-include-email)
+docker login --username AWS --password $(aws ecr get-login-password --region $AWS_REGION) 174232317769.dkr.ecr.$AWS_REGION.amazonaws.com
 
+docker-compose pull
 docker-compose up -d
 
 now=$(date +%s)
